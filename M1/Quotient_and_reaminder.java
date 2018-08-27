0d7869c3066e51a3c6ee3535edcfe5572b176c62
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-27 20:04:15 +0530

    Quotient_and_reaminder.java

---
+++ M1/Quotient_and_reaminder.java
@@ -0,0 +1,8 @@
+public class Quotient_and_reaminder{
+	public static void main(String[] args){
+		int a = 10;
+		int b = 3;
+		System.out.println("Quotient is:"+(a/b));
+		System.out.println("Remainder is:"+(a%b));
+	}
+}
