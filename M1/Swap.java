ebb6f1bb65d3f29479fde30af55c5387aa9033e2
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-27 20:06:26 +0530

    Swap.java

---
+++ M1/Swap.java
@@ -0,0 +1,13 @@
+public class Swap{
+	public static void main(String[] args)
+	{
+		int a = 5;
+		int b = 7;
+		int temp;
+		temp = a;
+		a = b;
+		b = temp;
+		System.out.println(a);
+		System.out.println(b);
+	}
+}
