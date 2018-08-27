e1e6dfb03251c2a305ce4a63a8fab9fb9bebad2a
shravyamutyapu <shravyamutyapu1840@msitprogram.net>
2018-08-27 19:52:20 +0530

    Degreetofarenheit.java

---
+++ M1/Degreetofarenheit.java
@@ -0,0 +1,10 @@
+import java.util.*;
+class Degrees_to_farenheit{
+	public static void main(String[] args){
+		double degree = 5.0;
+		double fahrenheit;
+		fahrenheit = degree*(9.0/5.0)+32;
+		System.out.println(fahrenheit);
+	}
+
+}
